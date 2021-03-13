736eb8d7eb7bf53e9b7284c54ba61c6baa163b33
Elena Fayle <efayle@my.wctc.edu>
2021-03-12 22:02:56 -0600

    Movie class

---
+++ Movie.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MediaLibraryLab
+{
+    // Movie class is derived from Media class
+    public class Movie : Media
+    {
+        public string director { get; set; }
+        // public TimeSpan runningTime { get; set; }
+        public TimeSpan runningTime { get; set; }
+
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nDirector: {director}\nRun time: {runningTime}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
+}
